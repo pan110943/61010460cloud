3048f8d734d07b920cfa51b572c76148ae57a8a9
Pan <pan.pornpinetpong@gmail.com>
2020-10-04 22:50:33 +0700

    61010460cloud

--- myweb/settings.py
+++ myweb/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'panweb',
+    'stock',
+
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'myweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR =(
+    os.path.join(BASE_DIR, "static"),
+)
 STATIC_URL = os.path.join(BASE_DIR,'static')
 # Activate Django-Heroku.
 django_heroku.settings(locals())
---
+++ panweb/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'frontend/index.html')
+def about(request):
+    return render(request, 'frontend/about.html')
+def contact(request):
+    return render(request, 'frontend/contact.html')
+    
